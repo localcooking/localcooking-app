3f34d77d1afa01d7ce2b0f4e1d89f04603a7b4ba
Athan Clark <athan.clark@gmail.com>
2021-07-10 14:44:36 -0400

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Main where
+
+
+import           Shpadoinkle                  (Html, JSM)
+import           Shpadoinkle.Backend.Snabbdom (runSnabbdom, stage)
+import           Shpadoinkle.Html
+import           Shpadoinkle.Run              (live, runJSorWarp, simple)
+
+
+view :: () -> Html m ()
+view _ = div_ [ "hello world" ]
+
+
+app :: JSM ()
+app = simple runSnabbdom () view stage
+
+
+dev :: IO ()
+dev = live 8080 app
+
+
+main :: IO ()
+main = do
+  putStrLn "\nhi, my name is localcooking-app"
+  putStrLn "happy point of view on https://localhost:8080\n"
+  runJSorWarp 8080 app
