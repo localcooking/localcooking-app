7e2baafe1af513ed668db69e6fba8f12c02b2fe1
Athan Clark <athan.clark@gmail.com>
2021-07-18 11:09:02 -0400

    actually inspecting user state now

--- src/Auth0.hs
+++ src/Auth0.hs
@@ -10,23 +10,34 @@ module Auth0 where
 import Auth0.Await (await, awaitThrow)
 
 import Shpadoinkle.Console (warn)
-import GHCJS.Types (JSString, JSVal)
-import GHCJS.Marshal (ToJSVal (toJSVal), fromJSValUnchecked)
+import GHCJS.Types (JSString, JSVal, isUndefined, isNull)
+import GHCJS.Marshal (ToJSVal (toJSVal), FromJSVal (fromJSVal, fromJSValUnchecked))
 import Control.Lens ((^.))
 import Control.Monad (void)
 import Control.Exception.Base (catch)
 import JavaScript.Object (create, unsafeSetProp)
-import Language.Javascript.JSaddle.Object (js0, js1)
+import Language.Javascript.JSaddle.Object (Object, js0, js1)
+import Language.Javascript.JSaddle.Classes.Internal (makeObject)
+import Language.Javascript.JSaddle.Types (ghcjsPure)
 
 
 default (JSString)
 
 foreign import javascript unsafe "createAuth0Client" createAuth0Client' :: JSVal -> IO JSVal
 
+-- | Sets the cache location for Auth0. See <https://github.com/auth0/auth0-spa-js#data-caching-options the docs> for details. The default value is 'Memory'. See also <https://github.com/auth0/auth0-spa-js#creating-a-custom-cache the docs on custom caches>.
+data CacheLocation = Memory | LocalStorage
+instance ToJSVal CacheLocation where
+  toJSVal x = toJSVal $ case x of
+    Memory -> "memory" :: JSString
+    LocalStorage -> "localstorage"
+
 data CreateAuth0Client = CreateAuth0Client
   { domain :: JSString
   , clientId :: JSString
   , redirectUri :: JSString
+  , cacheLocation :: Maybe CacheLocation
+  , useRefreshTokens :: Maybe Bool -- ^ See <https://github.com/auth0/auth0-spa-js#refresh-tokens the docs> for details.
   }
 instance ToJSVal CreateAuth0Client where
   toJSVal CreateAuth0Client{..} = do
@@ -37,6 +48,16 @@ instance ToJSVal CreateAuth0Client where
     unsafeSetProp "domain" domain' o
     unsafeSetProp "client_id" clientId' o
     unsafeSetProp "redirect_uri" redirectUri' o
+    case cacheLocation of
+      Nothing -> pure ()
+      Just cacheLocation' -> do
+        cacheLocation'' <- toJSVal cacheLocation'
+        unsafeSetProp "cacheLocation" cacheLocation'' o
+    case useRefreshTokens of
+      Nothing -> pure ()
+      Just useRefreshTokens' -> do
+        useRefreshTokens'' <- toJSVal useRefreshTokens'
+        unsafeSetProp "useRefreshTokens" useRefreshTokens'' o
     toJSVal o
 
 newtype Auth0 = Auth0 {getAuth0 :: JSVal}
@@ -64,15 +85,19 @@ handleRedirectCallback (Auth0 auth0) = do
 
 
 -- FIXME marshal into a haskell value
--- NOTE can be undefined, or an object of various vendor supported fields
-getUser :: Auth0 -> IO (Maybe JSVal)
+getUser :: FromJSVal a => Auth0 -> IO (Maybe a)
 getUser (Auth0 auth0) = do
   eRes <- await =<< auth0 ^. js0 "getUser"
   case eRes of
     Left e -> do
       warn @ToJSVal e
       pure Nothing
-    Right x -> pure $ Just x
+    Right x -> do
+      undef <- ghcjsPure $ isUndefined x
+      null <- ghcjsPure $ isNull x
+      if undef || null
+        then pure Nothing
+        else fromJSVal x
 
 
 getTokenSilently :: Auth0 -> IO (Maybe JSString)
--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,11 @@
   , RecordWildCards
   , TypeApplications
   , ExtendedDefaultRules
+  , NamedFieldPuns
+  , ScopedTypeVariables
+  , RankNTypes
+  , DeriveGeneric
+  , GeneralizedNewtypeDeriving
   #-}
 
 
@@ -18,36 +23,52 @@ import           Shpadoinkle.Console                        (warn)
 import           Control.Lens                               ((^.))
 import           Control.Monad                              (void)
 import           Control.Monad.IO.Class                     (MonadIO (liftIO))
+import           Control.DeepSeq                            (NFData (rnf))
+import           GHC.Generics                               (Generic)
 import qualified GHCJS.Types                                as T
 import           GHCJS.Foreign.Callback                     (asyncCallback1)
-import           GHCJS.Marshal                              (ToJSVal (toJSVal))
+import           GHCJS.Marshal                              (ToJSVal (toJSVal), FromJSVal (fromJSVal))
 import qualified Language.Javascript.JSaddle.Object         as JS
+import           System.IO.Unsafe                           (unsafePerformIO)
 
 default (T.JSString)
 
+foreign import javascript unsafe "$1 === $2" equalsJSVal :: T.JSVal -> T.JSVal -> Bool
+
+newtype UserInfo = UserInfo {getUserInfo :: JS.Object}
+  deriving (Generic, ToJSVal)
+instance NFData UserInfo where
+  rnf (UserInfo x) = seq x ()
+instance FromJSVal UserInfo where
+  fromJSVal x = Just . UserInfo <$> JS.makeObject x
+instance Eq UserInfo where
+  (UserInfo x) == (UserInfo y) = unsafePerformIO $
+    equalsJSVal <$> toJSVal x <*> toJSVal y
 
 
+data State = State
+  { loginInfo :: Maybe UserInfo
+  } deriving (Eq, Generic)
+instance NFData State
 
-view :: MonadIO m => Auth0 -> () -> Html m ()
-view auth0 _ =
-  div_
+view :: forall m. MonadIO m => Auth0 -> State -> Html m State
+view auth0 State{loginInfo} =
+  div_ $
     [ "hello world"
-    , button [onClickM_ (liftIO $ login auth0)] ["Login"]
-    , button [onClickM_ (liftIO $ logout auth0 Nothing)] ["Logout"]
-    , button [onClickM_ (liftIO $ checkLogin)] ["Check Login"]
-    ]
+    ] <> loginHeader
   where
-    checkLogin = do
-      void $ handleRedirectCallback auth0
-      -- warn @ToJSVal auth0
-      mUser <- getUser auth0
-
-      putStrLn "User Info"
-      warn @ToJSVal mUser
-
-
-app :: Auth0 -> JSM ()
-app auth0 = simple runSnabbdom () (view auth0) stage
+    loginHeader :: [Html m State]
+    loginHeader = case loginInfo of
+      Nothing ->
+        [ button [onClickM_ (liftIO $ login auth0)] ["Login"]
+        ]
+      Just userInfo ->
+        let checkLogin = do
+              putStrLn "User Info"
+              warn @ToJSVal userInfo
+        in  [ button [onClickM_ (liftIO $ logout auth0 Nothing)] ["Logout"]
+            , button [onClickM_ (liftIO $ checkLogin)] ["Check Login"]
+            ]
 
 
 main :: IO ()
@@ -58,6 +79,16 @@ main = do
     { domain = "localcooking-dev.us.auth0.com"
     , clientId = "Q0ITViiiUSflk71dTs8Zr9I3mfhwu35h"
     , redirectUri = "https://localcooking.com"
+    , cacheLocation = Nothing
+    , useRefreshTokens = Nothing
     }
 
-  runJSorWarp 8080 (app auth0)
+  void $ handleRedirectCallback auth0
+  mUser <- getUser auth0
+
+  runJSorWarp 8080 $
+    let initialState :: State
+        initialState = State
+          { loginInfo = mUser
+          }
+    in  simple runSnabbdom initialState (view auth0) stage
